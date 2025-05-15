109efc2a62ab5540d674fde6880e7f402b161166
iamzidan <zidann256@gmail.com>
2025-05-15 16:01:46 +0300

    firebase linked

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,12 +1,26 @@
 import 'package:auth_flow/features/auth/presentation/screen/forgot_password_view.dart';
 import 'package:auth_flow/features/auth/presentation/screen/log_in_View.dart';
 import 'package:auth_flow/features/auth/presentation/screen/sign_up_view.dart';
+import 'package:auth_flow/features/profile/presentation/cubit/auth_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 Map<String, WidgetBuilder> buildAppRoutes() {
   return {
-    '/login': (context) => const LogInView(),
-    '/forgotpass': (context) => const ForgotPasswordView(),
-    '/signup': (context) => const SignUpView(),
+    '/login':
+        (context) => BlocProvider(
+          create: (context) => AuthCubit(),
+          child: const LogInView(),
+        ),
+    '/forgotpass':
+        (context) => BlocProvider(
+          create: (context) => AuthCubit(),
+          child: const ForgotPasswordView(),
+        ),
+    '/signup':
+        (context) => BlocProvider(
+          create: (context) => AuthCubit(),
+          child: const SignUpView(),
+        ),
   };
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:auth_flow/core/database/cache/cache_helper.dart';
 import 'package:auth_flow/core/routes/app_router.dart';
 import 'package:auth_flow/core/services/service_locator.dart';
 import 'package:auth_flow/features/onboarding/presentation/screen/onboarding_view.dart';
+import 'package:auth_flow/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   setupServiceLocator();
   await getIt<CacheHelper>().init();
   runApp(const Appso());
